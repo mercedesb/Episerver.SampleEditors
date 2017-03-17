72b4a843ad6d4ec7c75236241056f8a3f343ce95
mercedesb <mercedesbernard@live.com>
2017-03-17 14:03:57 -0500

    added enum selection attributes

---
+++ SampleEditors/Models/Attributes/EnumSelectOneAttribute.cs
@@ -0,0 +1,29 @@
+﻿using EPiServer.Shell.ObjectEditing;
+using SampleEditors.Models.Selection;
+using System;
+
+namespace SampleEditors.Models.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	public class EnumSelectOneAttribute : SelectOneAttribute
+	{
+		public EnumSelectOneAttribute(Type enumType)
+		{
+			EnumType = enumType;
+		}
+
+		public Type EnumType { get; set; }
+
+		public override Type SelectionFactoryType
+		{
+			get
+			{
+				return typeof(EnumSelectionFactory<>).MakeGenericType(EnumType);
+			}
+			set
+			{
+				base.SelectionFactoryType = value;
+			}
+		}
+	}
+}
